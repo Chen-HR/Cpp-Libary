6e55a68a0494c19f3263ab185b7295faa633d38f
Chen-HuangRen <tn918419@gmail.com>
2023-06-05 09:42:38 +0800

    Make basic functions. Some marked "Runtime_Error".

--- Array/Object/Array.cpp
+++ Array/Object/Array.cpp
@@ -3,31 +3,193 @@
 #endif //class_Array_cpp
 #ifndef class_Array_cpp
   #define class_Array_cpp
-  template <typename DataType, typename LengthType> Array<DataType, LengthType>::Array() :DataPointer{nullptr}, DataLength{0} {}
-  template <typename DataType, typename LengthType> Array<DataType, LengthType>::Array(LengthType Length) :DataPointer{new DataType[Length]}, DataLength{Length} {}
-  template <typename DataType, typename LengthType> DataType Array<DataType, LengthType>::setItem(LengthType Index, DataType Data)
+  #include <memory> // std::make_shared()
+  #include <iostream> // std::ostream
+  #include <sstream> // std::ostringstream
+  #include <string> // std::string
+  #include <cstring> // std::memcpy
+  #define std_memcpy(dest, src) std::memcpy(dest, src, sizeof(src))
+  #define std_memcpy_length(dest, src, length) std::memcpy((dest), (src), sizeof(decltype((src)))*length)
+  template <typename Type, typename Size> Array<Type, Size>::Array() 
+    :_start{nullptr}, _length{0} 
+    {}
+  template <typename Type, typename Size> Array<Type, Size>::Array(Size length) 
+    // :_start{new Type[length]}, _length{length} 
+    // : _start{std::make_shared<Type[]>(length)}, _length{length}
     {
-      *(this->DataPointer+Index) = Data;
-      return Data;
+      // try
+      //   {
+      //     this->_start = new Type[length];
+      //   }
+      // catch(const std::exception& exception)
+      //   {
+      //     std::cerr << exception.what() << std::endl;
+      //   }
+      // this->_length = length;
+      // std::clog<<"Construct a new Array object (Configure a set of spaces, uninitialized)"<<std::endl;
+      this->_length = length;
+      try 
+        {
+          this->_start = new Type[length]; // ERROR
+        } 
+      catch (const std::bad_alloc& error) 
+        {
+          std::cerr << error.what() << std::endl;
+          // Handle memory allocation failure
+        }
     }
-  template <typename DataType, typename LengthType> DataType Array<DataType, LengthType>::getItemContent (LengthType Index)
+  template <typename Type, typename Size> Array<Type, Size>::Array(Array<Type, Size>& other, bool createCopy) 
     {
-      return *(this->DataPointer+Index);
+      this->set_Array(other, createCopy);
     }
-  template <typename DataType, typename LengthType> DataType* Array<DataType, LengthType>::getItemAddress (LengthType Index)
+  template <typename Type, typename Size> Array<Type, Size>::Array(Type* _start, Size length, bool createCopy) 
     {
-      return (this->DataPointer+Index);
+      this->set_Array(_start, length, createCopy);
     }
-  template <typename DataType, typename LengthType> DataType* Array<DataType, LengthType>::operator[] (LengthType index)
+  template <typename Type, typename Size> Array<Type, Size>::~Array() 
     {
-      return (this->DataPointer+index);
+      // // std::clog << "try: delete this->_start" << std::endl;
+      // try
+      //   {
+      //     delete[] this->_start;
+      //     // std::clog << "try: success" << std::endl;
+      //   }
+      // catch(const std::exception& exception)
+      //   {
+      //     // std::clog << "try: fail: "  << exception.what() << std::endl;
+      //   }
     }
-  // template <typename DataType, typename LengthType> DataType Array<DataType, LengthType>::operator[] (LengthType index)
+  template <typename Type, typename Size> inline Array<Type, Size>& Array<Type, Size>::set_Array(Array<Type, Size>& other, bool createCopy)
+    {
+      // std::clog<<"this->_length: "<<this->_length<<std::endl;
+      this->_length = other._length;
+      // std::clog<<"this->_length: "<<this->_length<<std::endl;
+      // std::clog<<"this->_start: "<<this->_start<<std::endl;
+      if (createCopy)
+        {
+          this->_start = new Type[other._length];
+          // this->_start = std::make_shared<Type[]>(other._length);
+          std_memcpy_length(this->_start, other._start, other._length);
+        }
+      else this->_start = other._start;
+      // std::clog<<"this->_start: "<<this->_start<<std::endl;
+      return *this;
+    }
+  template <typename Type, typename Size> inline Array<Type, Size>& Array<Type, Size>::set_Array(Type* start, Size length, bool createCopy)
+    {
+      // std::clog<<"this->_length: "<<this->_length<<std::endl;
+      this->_length = length;
+      // std::clog<<"this->_length: "<<this->_length<<std::endl;
+      // std::clog<<"this->_start: "<<this->_start<<std::endl;
+      if (createCopy)
+        {
+          this->_start = new Type[length];
+          // this->_start = std::make_shared<Type[]>(length);
+          std_memcpy_length(this->_start, start, length);
+        }
+      else this->_start = _start;
+      // std::clog<<"this->_start: "<<this->_start<<std::endl;
+      return *this;
+    }
+  template <typename Type, typename Size> inline Size Array<Type, Size>::get_Length()
+    { return this->_length; }
+  template <typename Type, typename Size> inline Array<Type, Size>& Array<Type, Size>::set_Length(Size length)
+    {
+      Array<Type, Size> tmp(length);
+      std_memcpy_length(tmp._start, this->_start, ((this->_length<tmp._length)?(this->_length):(tmp._length)));
+      // this->set_Array(tmp);
+      try { delete[] this->_start; }
+      catch(const std::exception& exception) {}
+      this->set_Array(tmp);
+      return *this;
+    }
+  template <typename Type, typename Size> inline Size Array<Type, Size>::Length()
+    { return this->get_Length(); }
+  template <typename Type, typename Size> inline Size Array<Type, Size>::Length(Size length)
+    { return this->set_Length(length)._length; }
+  template <typename Type, typename Size> inline Array<Type, Size>& Array<Type, Size>::set_ItemContent(Size index, Type data, bool reverse)
+    {
+      if (index < 0 || index >= this->_length) throw std::out_of_range("Index out of bounds");
+      *(this->_start+index) = data;
+      return *(this);
+    }
+  template <typename Type, typename Size> inline Type Array<Type, Size>::get_ItemContent (Size index, bool reverse)
+    {
+      if (index < 0 || index >= this->_length) throw std::out_of_range("Index out of bounds");
+      return *(this->_start+index);
+    }
+  template <typename Type, typename Size> inline Type* Array<Type, Size>::get_ItemAddress (Size index, bool reverse)
+    {
+      if (index < 0 || index >= this->_length) throw std::out_of_range("Index out of bounds");
+      return (this->_start+index);
+    }
+  template <typename Type, typename Size> Array<Type, Size> Array<Type, Size>::subArray(Size index, Size length, bool createCopy)
+    {
+      if (index < 0 || index+length >= this->_length) throw std::out_of_range("Index out of bounds");
+      Array<Type, Size> tmp(length);
+      if (createCopy) 
+        std_memcpy_length(tmp._start, this->_start+index, length);
+        // for (Size i=0; i<length; i++) *(tmp._start+i) = *(this->_start+index+i);
+      else tmp._start = this->_start+index;
+      return tmp;
+    }
+  template <typename Type, typename Size> Array<Type, Size> Array<Type, Size>::erase(Size index, Size length)
+    {
+      if (index < 0 || index+length >= this->_length) throw std::out_of_range("Index out of bounds");
+      Array<Type, Size> tmp(this->_length-length);
+      std_memcpy_length(tmp._start, this->_start, index);
+      std_memcpy_length(tmp._start+index, this->_start+index+length, this->_length-index-length);
+      return tmp;
+    }
+  // template <typename Type, typename Size> Array<Type, Size> Array<Type, Size>::inserted(Size index, Type data)
   //   {
-  //     return *(this->DataPointer+index);
+  //     const Size length = 1;
+  //     if (index < 0 || index > this->_length) throw std::out_of_range("Index out of bounds");
+  //     Array<Type, Size> tmp(this->_length+length);
+  //     if (index!=0) std_memcpy_length(tmp._start, this->_start, index);
+  //     std_memcpy_length(tmp._start+index, &data, length);
+  //     if (index!=this->_length) std_memcpy_length(tmp._start+index+length, this->_start+index, this->_length-index);
+  //     return tmp;
   //   }
-  // template <typename DataType, typename LengthType> DataType* Array<DataType, LengthType>::operator() (LengthType index)
+
+  template <typename Type, typename Size> inline Type& Array<Type, Size>::operator[] (Size index)
+    {
+      if (index < 0 || index >= this->_length) throw std::out_of_range("Index out of bounds");
+      return *(this->_start+index);
+    }
+  
+  // template <typename Type, typename Size> inline std::ostream& operator<< (std::ostream& outStream, Array<Type, Size>& array)
   //   {
-  //     return (this->DataPointer+index);
+  //     outStream<<"[";
+  //     for (size_t i=0; i<array.Length(); outStream<<array[i++])
+  //       if (i) outStream<<", ";
+  //     outStream<<"]";
+  //     return outStream;
   //   }
+  template <typename Type, typename Size> inline std::string Array<Type, Size>::to_string () const 
+    {
+      std::ostringstream buffer; buffer.clear();
+      try
+        {
+          buffer<<"[";
+          // std::clog<<"[";
+          for (size_t i=0; i<this->_length; i++) 
+            {
+              // std::clog<<buffer.str()<<std::endl;
+              if (i!=0) buffer<<", "; // ERROR
+              // if (i!=0) std::clog<<", ";
+              buffer<<*(this->_start+i);
+              // std::clog<<*(this->_start+i);
+            }
+          buffer<<"]";
+          // std::clog<<"]";
+        }
+      catch(const std::exception& exception) { std::cerr << "Exception occurred during stream insertion: " << exception.what() << std::endl; }
+      return buffer.str();
+    }
+  // template <typename Type, typename Size> inline const char* Array<Type, Size>::to_cstring () const 
+  //   {
+  //     return this->to_string().c_str();
+  //   }
+
 #endif //class_Array_cpp
--- Array/Object/Array.test.cpp
+++ Array/Object/Array.test.cpp
@@ -3,26 +3,61 @@
 #include <ctime>
 #include "Array.hpp"
 
-
 int main(int argc, const char** argv)
   {
-    std::cout<<"Integer size: "<<sizeof(size_t     )<<std::endl;
-    std::cout<<"Random  size: "<<sizeof(std::rand())<<std::endl;
-    std::cout<<"Integer Max : "<<SIZE_MAX<<std::endl;
-    std::cout<<"Random  Max : "<<RAND_MAX<<std::endl;
+    size_t Length=8;
+
+    Array<size_t, size_t> array(Length);
+    for (size_t i=0; i<Length; i++) array[i] = i;
+    std::cout<<"array: "<<array.to_string()<<std::endl;
+
+    Array<size_t, size_t> copy(array);
+    std::cout<<"copy: "<<copy.to_string()<<std::endl;
+
     std::cout<<std::endl;
     
-    size_t DataLength; std::cout<<"DataLength: "; std::cin>>DataLength;
-    Array<size_t, size_t> array(DataLength);
+    std::cout<<"array: "<<array.Length(Length<<1)<<": "<<array.to_string()<<std::endl;
+    std::cout<<"copy: "<<copy.Length(Length>>1)<<": "<<copy.to_string()<<std::endl;
+
     std::cout<<std::endl;
 
+    Array<char, size_t> string_number(17); string_number[16] = '\0';
+    for (size_t i=0; i<10; i++) string_number[   i] = '0'+i;
+    for (size_t i=0; i< 6; i++) string_number[10+i] = 'a'+i;
+    std::cout<<"string_number: "<<string_number.to_string()<<std::endl;
+    std::cout<<"string_number: "<<string_number.get_ItemAddress(0)<<std::endl;
+
+    Array<char, size_t> string_alpha(27); string_alpha[26] = '\0';
+    for (size_t i=0; i<26; i++) string_alpha[i] = 'a'+i;
+    std::cout<<"string_alpha: "<<string_alpha.to_string()<<std::endl;
+    std::cout<<"string_alpha: "<<string_alpha.get_ItemAddress(0)<<std::endl;
+
     std::srand(std::time(nullptr));
-    for (size_t i=0; i<DataLength; i++) 
-      array.setItem(i, std::rand());
+    Array<char, size_t> string_random(65); string_random[64] = '\0';
+    for (size_t i=0; i<64; i++) string_random[i] = string_number[std::rand()%16];
+    std::cout<<"string_random: "<<string_random.to_string()<<std::endl;
+    std::cout<<"string_random: "<<string_random.get_ItemAddress(0)<<std::endl;
 
-    std::cout<<"Data: ["; 
-    for (size_t i=0; i<DataLength; i++) 
-      std::cout<<array[i]<<", "; 
-    std::cout<<"\b\b]"<<std::endl<<std::endl;
+    std::cout<<std::endl;
 
+    std::cout<<"string_number: "<<string_number.to_string()<<std::endl;
+    Array<char, size_t> string_number_subArray_0_16(string_number.subArray(0, 16)); // string_number_subArray_0_16.set_ItemContent(0, '\0', true);
+    std::cout<<"string_number.subArray(0, 16): "<<string_number_subArray_0_16.get_ItemAddress(0)<<": "<<string_number_subArray_0_16.to_string()<<std::endl;
+    Array<char, size_t> string_number_subArray_4__8(string_number.subArray(4,  8)); // string_number_subArray_4__8.set_ItemContent(0, '\0', true);
+    std::cout<<"string_number.subArray(4,  8): "<<string_number_subArray_4__8.get_ItemAddress(0)<<": "<<string_number_subArray_4__8.to_string()<<std::endl;
+
+    std::cout<<std::endl;
+
+    std::cout<<"string_number: "<<string_number.to_string()<<std::endl;
+    Array<char, size_t> string_number_erase_0_16(string_number.erase(0, 16));
+    Array<char, size_t> string_number_erase_4__8(string_number.erase(4,  8));
+    std::cout<<"string_number.erase(0, 16): "<<string_number_erase_0_16.to_string()<<std::endl;
+    std::cout<<"string_number.erase(4,  8): "<<string_number_erase_4__8.to_string()<<std::endl;
+
+    // std::cout<<std::endl;
+
+    // std::cout<<"string_number: "<<string_number.to_string()<<std::endl;
+    // std::cout<<"string_number.inserted( 0, 'z'): "<<string_number.inserted( 0, 'z').to_string()<<std::endl;
+    // std::cout<<"string_number.inserted( 8, 'z'): "<<string_number.inserted( 8, 'z').to_string()<<std::endl;
+    // std::cout<<"string_number.inserted(16, 'z'): "<<string_number.inserted(16, 'z').to_string()<<std::endl;
   }
